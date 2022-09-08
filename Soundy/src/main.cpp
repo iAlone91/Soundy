6eed4dc4c4fd4203085f2e7a3af3fba2f8b38fb1
Christoffer Persson <kristoffer_91@live.se>
2022-09-08 13:39:28 +0200

    removed SDL Submodule

--- Soundy/src/main.cpp
+++ Soundy/src/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <SDL.h>
 
-int main()
+int main(int argc, char** argv)
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	std::cout << "Hello SDL\n";
+
+	SDL_Quit();
 
 	return 0;
 }
